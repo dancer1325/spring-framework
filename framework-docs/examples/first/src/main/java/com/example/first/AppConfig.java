79a12ffadc34170548f624e7f1fa9f060cc79a0c
alfredo-toledano <alfredo.toledano@allianzdirect.es>
2024-07-15 07:29:09 +0200

    doc(docs.example.first): add note

--- framework-docs/examples/first/src/main/java/com/example/first/AppConfig.java
+++ framework-docs/examples/first/src/main/java/com/example/first/AppConfig.java
@@ -3,6 +3,7 @@ package com.example.first;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+// TODO: NOT required to annotate with @EnableWebMVC ?
 public class AppConfig implements WebMvcConfigurer {
 
     @Override
